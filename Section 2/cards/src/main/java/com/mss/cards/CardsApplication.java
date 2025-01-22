ab003067ed3a10e07b5052ecf94bb9147c61ff40
Md. Tanver Ahammed <67496397+Tanver-Ahammed@users.noreply.github.com>
2025-01-22 10:25:37 +0600

    Update v-3.4.1 (#1)
    * spring-boot version update
    
    * version and code update

---
+++ Section 2/cards/src/main/java/com/mss/cards/CardsApplication.java
@@ -0,0 +1,42 @@
+package com.mss.cards;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+/*@ComponentScans({ @ComponentScan("com.mss.cards.controller") })
+@EnableJpaRepositories("com.mss.cards.repository")
+@EntityScan("com.mss.cards.model")*/
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Cards microservice REST API Documentation",
+                description = "M-Bank Cards microservice REST API Documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "Md. Tanver Ahammed",
+                        email = "tanver@mss.com",
+                        url = "https://github.com/Tanver-Ahammed"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://github.com/Tanver-Ahammed"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "M-Bank Cards microservice REST API Documentation",
+                url = "https://github.com/Tanver-Ahammed"
+        )
+)
+public class CardsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardsApplication.class, args);
+    }
+}
